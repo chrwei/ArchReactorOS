3ecf12668bcb861b0ad1fb4fc5657e07f76049b2
olleolleolle <olle.jonsson@gmail.com>
2010-11-29 17:26:56 +0100

    Move script to app script from free SCRIPT blocks

--- aros.js
+++ aros.js
@@ -2,6 +2,30 @@
 /*jslint browser:true */
 
 /**
- * ArchReactorOS script
- */
+* ArchReactorOS script
+*/
 var AROS = AROS || {};
+
+
+// To load on document ready
+$(function() {
+	var dumy = '';
+	$(".view_detail").tooltip({
+		delay: 200,
+		position: 'mouse',
+		top: -15,
+		left: 5,
+		bodyHandler: function() {
+			$.ajax({
+				url: $(this).attr('ajaxurl'),
+				type:'post',
+				success:function(msg){
+					dumy = msg; 
+				},
+				async: false
+			});
+			return dumy;
+		},
+		showCFG_SITE_URL: false
+	});
+});
